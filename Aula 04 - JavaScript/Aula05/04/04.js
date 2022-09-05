4f9af0c77b62ecc34a696d8b4718ddd25e88ee9c
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-09-05 17:26:46 -0300

    Update 04.js

--- Aula 04 - JavaScript/Aula05/04/04.js
+++ Aula 04 - JavaScript/Aula05/04/04.js
@@ -23,3 +23,17 @@ const amigos4 = amigos3.filter(aux => aux.idade >=21 || aux.nome.includes("B"))
 for(const amigo of amigos4){
     console.log(`${amigo.nome} idade:${amigo.idade} anos`);
 }
+
+
+//Reduce
+
+const amigos5  = [{nome:"Ana",idade:20},
+                 {nome:"Bruno",idade:17},
+                 {nome:"Cátia",idade:25}
+                ]
+
+const soma = amigos5.reduce((acumulador,amigo)=> acumulador + amigo.idade,0)
+const todos = amigos.reduce((acumulador,amigo)=> acumulador + amigo.nome + " "," ")
+
+console.log(`Soma: ${soma}`);
+console.log(`Todos:${todos}`);
