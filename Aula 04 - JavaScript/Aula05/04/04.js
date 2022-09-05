2373eb3aee79b03bf75c4aaf8aa13ee0f93569c0
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-09-05 17:20:38 -0300

    Update 04.js

--- Aula 04 - JavaScript/Aula05/04/04.js
+++ Aula 04 - JavaScript/Aula05/04/04.js
@@ -1,3 +1,5 @@
+
+//Map
 const amigos  = [{nome:"Ana",idade:20},
                  {nome:"Bruno",idade:17},
                  {nome:"Cátia",idade:25}
@@ -7,4 +9,17 @@ const amigos2 = amigos.map(aux =>({nome: aux.nome,nasc:2022 - aux.idade}))
 
 for(const amigo of amigos2){
     console.log(`${amigo.nome} - Nasceu em ${amigo.nasc}`);
+}
+
+//Filter
+
+const amigos3  = [{nome:"Ana",idade:20},
+                 {nome:"Bruno",idade:17},
+                 {nome:"Cátia",idade:25}
+                ]
+
+const amigos4 = amigos3.filter(aux => aux.idade >=21 || aux.nome.includes("B"))
+
+for(const amigo of amigos4){
+    console.log(`${amigo.nome} idade:${amigo.idade} anos`);
 }
