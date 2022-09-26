3ef1734a3438d92615de226a9033d1cb24bbfde0
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-09-26 17:32:51 -0300

    updatee

--- Aula 04 - JavaScript/Aula07/04/js/04.js
+++ Aula 04 - JavaScript/Aula07/04/js/04.js
@@ -3,26 +3,44 @@ const resp = document.querySelector("pre")
 
 const itens = []
 
-frm.rbPizza.AddEventListener("click",()=>{
+frm.rbPizza.addEventListener("click",()=>{
   frm.inBebida.className = "oculta"
   frm.inPizza.className = "exibe"
 })
 
-frm.rbBebida.AddEventListener("click",()=>{
+frm.rbBebida.addEventListener("click",()=>{
   frm.inPizza.className = "oculta"
   frm.inBebida.className = "exibe"
 })
 
-frm.inDetalhes.AddEventListener("focus",()=>{
+frm.inDetalhes.addEventListener("focus",()=>{
   if(frm.rbPizza.checked){
     const pizza = frm.inPizza.value
 
-    const num = pizza= "media"? 2 :pizza =="grande"? 3: 4
+    const num = pizza== "media"? 2 :pizza =="grande"? 3: 4
 
     frm.inDetalhes.placeholder = `Até ${num} sabores`
   }
 })
 
-frm.inDetalhes.AddEventListener("blur",()=>{
+frm.inDetalhes.addEventListener("blur",()=>{
   frm.inDetalhes.placeholder = ""
+})
+
+frm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let produto
+    if(frm.rbPizza.checked){
+      const num = frm.inPizza.selectedIndex
+      produto = frm.inPizza.options[num].text
+    }else{
+      const num = frm.inBebida.selectedIndex
+      produto = frm.inBebida.options[num].text
+    }
+    const detalhes = frm.inDetalhes.value
+    itens.push(produto + " (" + detalhes + ")")
+    resp.innerText = itens.join("\n")
+
+    frm.reset()
+    frm.rbPizza.dispatchEvent(new Event("click"))
 })
