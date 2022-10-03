17017e669c33b5b69fdecedef9b45b700b58e720
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-10-03 17:47:17 -0300

    Update SQLQuery001.sql

--- Aula 03 - BancoDados/Aula10/SQLQuery001.sql
+++ Aula 03 - BancoDados/Aula10/SQLQuery001.sql
@@ -34,4 +34,65 @@ ALTER Database bkBankAula10
 
 ---------
 
+USE bkBankAula10;
 
+GO
+CREATE SCHEMA Sales;
+
+GO
+CREATE SCHEMA HumanResources;
+
+GO
+----------------------------------------
+Use bkBankAula10
+
+Create Table HumanResources.Address(
+AddressID int Constraint nnAddressAddressID NOT NULL IDENTITY(1,1),
+StreetAddress	Varchar(125) Constraint nnAddressStreetAddress NOT NULL,
+StreetAddress2  Varchar(75) Constraint nnAddressStreetAddress2 NOT NULL,
+City			Varchar(100)Constraint nnAddressCity NOT NULL,
+State			CHAR(2)Constraint nnAddressState NOT NULL,
+EmployeeID		INT constraint nnAddressEmployed not null
+)ON bkBankAula10Group1;
+
+--
+select * from sys.sysconstraints 
+select * from sys.foreign_keys
+select * from sys.key_constraints -- pk/uk
+select * from sys.check_constraints
+select * from sys.default_constraints
+---------------
+CREATE TABLE HumanResources.Employee(
+	EmployeeID INT CONSTRAINT nnEmployeeEmployeeID NOT NULL IDENTITY(1,1),
+	FirstName VARCHAR(50) CONSTRAINT nnEmployeeFirstName NOT NULL,
+	MiddleName VARCHAR(50) NULL,
+	LastName VARCHAR(50) CONSTRAINT nnEmployeeLastName NOT NULL
+) ON bkBankAula10Group1;
+
+ALTER TABLE HumanResources.Employee
+	ADD GENDER CHAR(1) CONSTRAINT nnEmployeeGender NOT NULL;
+
+--------
+
+use bkBankAula10
+
+ALTER TABLE HumanResources.Employee
+	ADD FullName as LastName + ' ' + FirstName;
+
+--
+Use bkBankAula10
+	ALTER TABLE HumanResources.Employee
+	ADD	 SocialSecurityNumber Varchar(10)
+		CONSTRAINT nnEmployeeSocialSecurityNumber NOT NULL;
+
+	ALTER TABLE HumanResources.Employee
+		ADD Active BIT
+		CONSTRAINT nnEmployeeActive NOT NULL;
+
+	ALTER TABLE HumanResources.Employee
+	 ADD CONSTRAINT pkHumanResourceEmployeeID
+		PRIMARY KEY(EmployeeID);
+	
+		ALTER TABLE HumanResources.Address
+			 ADD CONSTRAINT pkHumanResourceAddressID
+			PRIMARY KEY(AddressID);
