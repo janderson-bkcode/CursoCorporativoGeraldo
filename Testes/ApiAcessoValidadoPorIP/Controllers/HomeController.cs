0320deb9b99bb2755719b3208f5aebe481e64ddb
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-10-21 11:50:46 -0300

    update blockIPApi project

---
+++ Testes/ApiAcessoValidadoPorIP/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAcessoValidadoPorIP.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("Unblocked")]
+        public string Unblocked()
+        {
+            return "Unblocked access";
+        }
+        [HttpGet("blocked")]
+        public string blocked()
+        {
+            return "blocked access";
+        }
+
+    }
+}
