02c087f4906e1c5c213e36ba610a49da574a88bc
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-11-16 15:41:31 -0300

    Update Aula16.sql

--- Aula 03 - BancoDados/Aula16/Aula16.sql
+++ Aula 03 - BancoDados/Aula16/Aula16.sql
@@ -3,6 +3,7 @@
 USE MyAdventureWorks
 
 --CRIANDO A VIEW DDL
+GO
 CREATE VIEW dbo.vwEmployeeInformation
 AS 
 SELECT p.Title,p.FirstName,p.MiddleName,
@@ -10,7 +11,7 @@ SELECT p.Title,p.FirstName,p.MiddleName,
 FROM Person.Person p 
 INNER JOIN HumanResources.Employee e
     ON (p.BusinessEntityID = e.BusinessEntityID);
-
+GO
 -- INVOCANDO A VIEW
 SELECT *
 FROM vwEmployeeInformation
@@ -73,11 +74,86 @@ AS
 BEGIN
     --DECLARA A VARIAVEL DE RETORNO
     DECLARE @Age INT
-    --ADICIONE AS INTRUÇÕES T-SQL PARA CALCULAR O VALOR DE RETORNO 
+    --ADICIONE AS INSTRUÇÕES T-SQL PARA CALCULAR O VALOR DE RETORNO 
     SELECT @Age = DATEDIFF(DAY,@BirthDate,GETDATE())
     --RETORNA O RESULTADO DA FUNÇÃO
     RETURN @Age
 END;
 GO
 
+--INVOCANDO A FUNÇÃO GetEmployeeAge
+SELECT
+    p.FirstName,p.LastName,e.BirthDate,
+    dbo.GetEmployeeAge(BirthDate) EmployeeAge
+FROM HumanResources.Employee e
+INNER JOIN Person.Person p
+ON (e.BusinessEntityID = p.BusinessEntityID)
 
+GO 
+-- ALTERANDO FUNÇÃO PAGE 35
+ALTER FUNCTION [dbo].[GetEmployeeAge](@BirthDate DATETIME)
+RETURNS INT
+AS
+BEGIN
+--DECLARA A VARIVAVEL DE RETORNO
+DECLARE @Age int
+--adicione as instruções T-SQL para calcular o valor de retorno
+SELECT @Age = DATEDIFF(Year,@BirthDate,GETDATE())
+
+--RETORNA O RESULTADO DA FUNÇÃO
+RETURN @Age
+END;
+
+GO
+    SELECT TOP(10)
+        p.FirstName,p.LastName,e.BirthDate,
+        dbo.GetEmployeeAge(BirthDate) EmployeeAge
+    FROM HumanResources.Employee e
+    INNER JOIN Person.Person p
+    ON (e.BusinessEntityID = p.BusinessEntityID)
+GO
+
+GO 
+    DROP FUNCTION dbo.GetEmployeeAge;
+GO
+
+--PAGE 40
+GO 
+    IF(OBJECT_ID('dbo.GetEmployeeAge')) IS NOT NULL
+        DROP FUNCTION dbo.GetEmployeeAge
+    GO
+
+    CREATE FUNCTION [dbo].[GetEmployeeAge](
+        @BirthDate DATETIME = '26/05/1972',--DEFAULT
+        @Temp DATETIME = NULL --OPCIONAL
+    )
+    RETURNS INT
+    AS
+    BEGIN
+    --DECLARA VARIÁVEL DE RETORNO
+    DECLARE @Age INT
+    --ADICIONA AS INSTRUÇÕES T-SQL PARA CALCULAR O VALOR DE RETORNO
+    SELECT @Age = DATEDIFF(YEAR,@BirthDate,GETDATE())
+    --RETORNA O RESULTADO DA FUNÇÃO
+    RETURN @Age
+    END;
+GO
+
+--INVOCANDO A FUNÇÃO DE DIVERSOS MANEIRAS
+
+--PARAMETRO DE ENTRADA ÚNICO 
+SELECT dbo.GetEmployeeAge(DEFAULT,NULL);
+SELECT dbo.GetEmployeeAge('26/05/1972','');
+SELECT dbo.GetEmployeeAge('26/05/1972',NULL);
+
+-- O PRIMEIRO PARAMETRO É PADRÃO E O SEGUNDO É DE ENTRADA
+SELECT dbo.GetEmployeeAge(DEFAULT,'10/01/1972');
+SELECT dbo.GetEmployeeAge('26/05/1972','10/01/1972');
+
+
+--
+GO
+    DECLARE @Age INT;
+    EXECUTE @Age = dbo.GetEmployeeAge @BirthDate = '31/07/1977';
+    SELECT @Age;
+GO
