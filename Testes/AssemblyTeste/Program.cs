5920a338ab4fe2b321275b84f679d756b82650d5
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2023-03-31 09:41:10 -0300

    Update Program.cs

--- Testes/AssemblyTeste/Program.cs
+++ Testes/AssemblyTeste/Program.cs
@@ -26,7 +26,77 @@ namespace AssemblyTeste
             var dataCriacao = File.GetCreationTime(assembly.Location);
             System.Console.WriteLine($"Data Criação {dataCriacao}");
 
+            System.Console.WriteLine(FormatEmbossingName("Janderson Barbosa"));
+            System.Console.WriteLine(FormatEmbossingName2("Janderson"));  System.Console.WriteLine(FormatEmbossingName2("Janderson Barbosa Gonçalves"));
         }
+           public static  string FormatEmbossingName(string FullName)
+        {
+            var nameParts = FullName.Trim().Split(' ');
+
+            var embossingName = nameParts[0];
+
+            for (int i = 1; i < nameParts.Length - 1; i++)
+            {
+                if (nameParts[i].Length > 2)
+                {
+                    embossingName += " " + nameParts[i].Substring(0, 1);
+                }
+            }
+
+            embossingName += " " + nameParts[nameParts.Length - 1];
+
+            if (embossingName.Length > 20)
+            {
+                embossingName = $"{nameParts[0]} {nameParts[nameParts.Length - 1]}";
+
+                if(embossingName.Length > 20)
+                {
+                    embossingName = embossingName.Substring(0, 20);
+                }                                
+            }
+
+            return embossingName.ToUpper();
+        }
+  public static string FormatEmbossingName2(string FullName)
+        {
+            var nameParts = FullName.Trim().Split(' ');
+
+            if (nameParts.Length == 1)
+            {
+                return FullName.Trim().ToUpper();
+            }
+
+            if (nameParts.Length > 3)
+            {
+                return $"{nameParts[0]} {nameParts[1][0]} {nameParts[nameParts.Length - 1]}".Trim().ToUpper();
+            }
+
+            var embossingName = nameParts[0];
+
+            for (int i = 1; i < nameParts.Length - 1; i++)
+            {
+                if (nameParts[i].Length > 2)
+                {
+                    embossingName += " " + nameParts[i].Substring(0, 1);
+                }
+            }
+
+            embossingName += " " + nameParts[nameParts.Length - 1];
+
+            if (embossingName.Length > 20)
+            {
+                embossingName = $"{nameParts[0]} {nameParts[nameParts.Length - 1]}";
+
+                if (embossingName.Length > 20)
+                {
+                    embossingName = embossingName.Substring(0, 20);
+                }
+            }
+
+            return embossingName.Trim().ToUpper();
+        }
+
+
     }
     
 }
